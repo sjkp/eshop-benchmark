221d5e2294205562e09a9cb3053a94f6f5259692
Simon J.K. Pedersen <mail@sjkp.dk>
2024-01-06 14:17:22 +0100

    release mode:

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,7 @@
 for runId in {1..10}
 do
     echo "Run $runId"
-    dotnet clean eShop.Web.slnf
-    dotnet build eShop.Web.slnf >> "macbook_m3pro_2_$runId.txt"
+    dotnet clean eShop.Web.slnf --configuration=release
+    dotnet build eShop.Web.slnf --configuration=release >> "macbook_m3pro_2_$runId.txt"
     echo "Complete $runId"
 done
